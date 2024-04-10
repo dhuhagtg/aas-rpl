3275968f79c13dbf7964de3dd03c2a4205596da3
lupacaratidur <king.wahrul@gmail.com>
2024-04-10 19:41:47 +0700

    repair: navbar dropdown

--- resources/js/Components/Common/navbar/Navbar.jsx
+++ resources/js/Components/Common/navbar/Navbar.jsx
@@ -10,13 +10,13 @@ const Navbar = () => {
     <>
 
       <div className="navbar bg-base-100">
-        <div className="navbar-start">
+        <div className="navbar-start" >
           <img src={Logo} alt="Logo" className="logo" />
-          <div className="dropdown">
+          <div className="dropdown z-50 z-index-9999">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </div>
-            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
               <li><a>Beranda</a></li>
               <li>
                 <a>Profil</a>
@@ -37,7 +37,7 @@ const Navbar = () => {
                 <a>Layanan</a>
                 <ul className="p-2">
                   <li><a>Pengajuan Surat</a></li>
-                  <li><a>Pengaduan Masyarakat</a></li>
+                  <li><a style={{ whiteSpace: 'nowrap' }}>Pengaduan Masyarakat</a></li>
                 </ul>
               </li>
 
@@ -49,23 +49,23 @@ const Navbar = () => {
           <a className="btn btn-ghost text-xl">Desa Margasana</a>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
+          <ul className="menu menu-horizontal px-1" >
             <li><a>Beranda</a></li>
             <li>
               <details>
                 <summary>Profil</summary>
-                <ul className="p-2">
-                  <li><a>Visi dan Misi</a></li>
-                  <li><a>Sejarah Desa</a></li>
-                  <li><a>Wilayah Desa</a></li>
+                <ul className="p-2" style={{ zIndex: '2' }}>
+                  <li><a style={{ whiteSpace: 'nowrap' }} >Visi dan Misi</a></li>
+                  <li><a style={{ whiteSpace: 'nowrap' }} >Sejarah Desa</a></li>
+                  <li><a style={{ whiteSpace: 'nowrap' }} >Wilayah Desa</a></li>
                 </ul>
               </details>
             </li>
             <li>
               <details>
                 <summary>Lembaga Desa</summary>
-                <ul className="p-2">
-                  <li><a>Pemerintah Desa</a></li>
+                <ul className="p-2" style={{ zIndex: '2' }}>
+                  <li><a style={{ whiteSpace: 'nowrap' }}>Pemerintah Desa</a></li>
                   <li><a>BPD</a></li>
                 </ul>
               </details>
@@ -73,9 +73,9 @@ const Navbar = () => {
             <li>
               <details>
                 <summary>Layanan</summary>
-                <ul className="p-2">
-                  <li><a>Pengajuan Surat</a></li>
-                  <li><a>Pengaduan Masyarakat</a></li>
+                <ul className="p-2" style={{ zIndex: '2' }}>
+                  <li><a style={{ whiteSpace: 'nowrap' }}>Pengajuan Surat</a></li>
+                  <li><a style={{ whiteSpace: 'nowrap' }}>Pengaduan Masyarakat</a></li>
                 </ul>
               </details>
             </li>
@@ -83,6 +83,7 @@ const Navbar = () => {
             <li><a>Galeri</a></li>
           </ul>
         </div>
+
         <div className="navbar-end">
           <a className="btn">Login</a>
         </div>
